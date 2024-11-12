c7557268a2c1aac9148937b472d1fe0002b5b8f2
cy-cheng <cycheng.dev@gmail.com>
2024-11-12 09:20:19 +0800

    update Vimtex config temporarily for hw

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,5 +5,6 @@ local opts = { noremap = true, silent = true }
 
 map("i", "jk", "<esc>", opts)
 
-map("n", "<leader>ll", ":VimtexCompile<CR>:!zathura %:r.pdf &<CR>", opts)
+map("n", "<leader>ll", ":VimtexCompile<CR>", opts)
+map("n", "<leader>oo", ":!zathura %:r.pdf &<CR><CR>", opts)
 
--- lua/plugins/config/completion.lua
+++ lua/plugins/config/completion.lua
@@ -1,50 +1,38 @@
--- plugins/config/completion.lua
-
-local cmp = require("cmp") 
-
--- Helper function to check if there are words before the cursor
-local has_words_before = function()
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
 cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
-
-    sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "buffer" },
-        { name = "path" },
-    }),
-	window = {
-		documentation = cmp.config.window.bordered(),
-	},
-    mapping = cmp.mapping.preset.insert({
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif has_words_before() then
-                cmp.complete()
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif vim.fn["vsnip#jumpable"](-1) == 1 then
-                vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>(vsnip-jump-prev)", true, true, true), "")
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    }),
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<S-Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
 })
 
