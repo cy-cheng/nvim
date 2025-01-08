84258c3a025aff529ff68dac50b6fabaf5cbeec5
cy-cheng <cycheng.dev@gmail.com>
2025-01-09 00:35:06 +0800

    change keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,9 +4,17 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 map("i", "jk", "<esc>", opts)
+map("i", "JK", "<esc>", opts)
 
-map("n", "<leader>ll", ":VimtexCompile<CR>", opts)
-map("n", "<leader>oo", ":!zathura %:r.pdf &<CR><CR>", opts)
+map("n", "H", "h", opts)
+map("n", "J", "j", opts)
+map("n", "K", "k", opts)
+map("n", "L", "l", opts)
+
+map("n", "<leader>l", ":VimtexCompile<CR>", opts)
+map("n", "<leader>e", ":VimtexErrors<CR>", opts)
+map("n", "<leader>o", ":!zathura %:r.pdf &<CR><CR>", opts)
+
 map('i', '<C-J>', "copilot#Accept('<CR>')", { expr = true, silent = true })
+map("n", "<leader>c", ":CopilotChatToggle<CR>", opts)
 
-map("n", "<leader>cc", ":CopilotChatToggle<CR>", opts)
