8c577ddcba6245b1377b7cfcf7bf8c371e285d64
cy-cheng <cycheng.dev@gmail.com>
2025-09-14 21:17:36 +0800

    make notification come from below

--- lua/plugins/notification.lua
+++ lua/plugins/notification.lua
@@ -1,8 +1,17 @@
 return {
     {
+        "rcarriga/nvim-notify",
+        config = function()
+            require("notify").setup({
+              top_down = false,
+            })
+        end
+    },
+    {
         "folke/noice.nvim",
         event = "VeryLazy",
         opts = {
+            top_down = false,
 
         },
         dependencies = {
