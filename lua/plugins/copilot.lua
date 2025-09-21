78c71cf873c402f925c29428604482aee126b24e
cy-cheng <cycheng.dev@gmail.com>
2025-09-21 21:34:28 +0800

    add copilot files

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,31 @@
+return {
+    {
+        "github/copilot.vim",
+        lazy = false,
+
+        config = function()
+            vim.g.copilot_no_tab_map = true
+        end,
+    },
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        opts = {
+            display = {
+                chat = {
+                    show_settings = true,
+                },
+            },
+            strategies = {
+                chat = {
+                    adapter = {
+                        name = "copilot",
+                        model = "claude-sonnet-4",
+                    },
+                },
+            },
+        }
+    }
+}
