da4337802a16e17fcec0f3d6554b9268cf407613
cy-cheng <cycheng.dev@gmail.com>
2025-10-15 10:10:35 +0800

    mamking copilot configs

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,32 +1,54 @@
 return {
-    {
-        "github/copilot.vim",
-        lazy = false,
+	{
+		"github/copilot.vim",
+		lazy = false,
 
-        config = function()
-            vim.g.copilot_no_tab_map = true
-
-        end,
-    },
-    {
-        "olimorris/codecompanion.nvim",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-        },
-        opts = {
-            display = {
-                chat = {
-                    show_settings = true,
-                },
-            },
-            strategies = {
-                chat = {
-                    adapter = {
-                        name = "copilot",
-                        model = "grok-code-fast-1",
-                    },
-                },
-            },
-        }
-    }
+		config = function()
+			vim.g.copilot_no_tab_map = true
+		end,
+	},
+	{
+		"olimorris/codecompanion.nvim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("codecompanion").setup({
+				memory = {
+					opts = {
+						chat = {
+							enabled = true,
+						},
+					},
+				},
+				display = {
+					chat = {
+						show_settings = true,
+					},
+				},
+				strategies = {
+					chat = {
+						adapter = {
+							name = "copilot",
+							model = "grok-code-fast-1",
+						},
+						variables = {
+							buffer = {
+								opts = {
+									max_lines = 200,
+								},
+							},
+						},
+						slash_commands = {
+							file = {
+								opts = {
+									max_lines = 200,
+								},
+							},
+						}
+					},
+				},
+			})
+		end,
+	},
 }
